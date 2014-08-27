f4462134d2d5504f4e644918a736846326120888
Andres Aravena <andres.aravena@dim.uchile.cl>
2014-08-27 19:13:51 -0400

    script y datos para ejemplo chico

--- create-small-example.R
+++ create-small-example.R
@@ -12,4 +12,3 @@ write.graph(g1, 'Input/RDB8.1/Prodoric/MEME/small.ncol', format="ncol")
 g0 <- read.graph("Input/RDB8.1/M3D/pearson/mrnet/100K.ncol", format="ncol", names=T, direct=F, weights="yes")
 g1 <- induced.subgraph(g0, V(g0)[ref.nodes])
 write.graph(g1, 'Input/RDB8.1/M3D/pearson/mrnet/small.ncol', format="ncol")
-
