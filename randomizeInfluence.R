9df9292b043d6993c7d0a8367fc30380d03bc814
Andres Aravena <andres.aravena@gmail.com>
2014-11-07 16:38:21 -0300

    cambio de modelo "nulo" de coexpresión. Ahora se basa en la misma distribución de grados

--- randomizeInfluence.R
+++ randomizeInfluence.R
@@ -1,30 +1,21 @@
 #! /usr/bin/env Rscript --vanilla
 
+library(methods)
 library(igraph)
 library(optparse)
 
 # Argument handling
 option_list <- list(
-  make_option(c("-N", "--nodes"),  help = "Filename of node names."),
-  make_option(c("-n", "--count"), action="store", default=10000,
-              help="Number of arcs to generate."),
-  make_option(c("-c", "--column"), action="store", default=2, type="integer",
-              help="Column id of node names Default: 2.")
+  make_option(c("-c", "--coexp"), help = "Filename of coexpressions."),
+  make_option(c("-n", "--count"), action="store", default=1000, help="Number of arcs to swap."),
+  make_option(c("-o", "--outfile"), help = "Output filename.", default="/dev/stdout")
 )
 
 opt.parser <- OptionParser(option_list = option_list, 
-                           description="Usage: reachability.R [options] net.file [...]")
-
-# arg <-strsplit("--nodes Input/RDB8.1/operon_names.txt --count 10000"," ")[[1]]
-# opts <- parse_args(opt.parser, args=arg)
+                           description="Usage: randomizeInfluence.R [options]")
 opts <- parse_args(opt.parser)
-ops <- read.table(opts$nodes, as.is=T)
-ops <- ops[, opts$column]
 
-size <- opts$count
-g <- erdos.renyi.game(length(ops), size, type="gnm")
-edges <- get.edges(g,E(g))
-ans <- data.frame(X=ops[edges[,1]], Y=ops[edges[,2]], Z=runif(size))
+g <- read.graph(opts$coexp, format="ncol")
 
-write.table(ans, file="",
-            sep="\t", quote=F, row.names=F, col.names=F)
+write.graph(rewire(g, niter=opts$count), opts$outfile, format="ncol")
+
