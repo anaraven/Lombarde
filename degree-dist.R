be3183feddac395acb1bf211bbd06cbad720180b
Andres Aravena <andres.aravena@gmail.com>
2015-11-17 10:22:32 +0100

    degree analysis using different degree definitions

--- degree-dist.R
+++ degree-dist.R
@@ -8,8 +8,8 @@ library(igraph)
 option_list <- list(
   make_option(c("--header"), action="store_true", default=FALSE,
               help="Show field names as header"),
-  make_option(c("-u","--undirected"), action="store_true", default=FALSE,
-              help="Input graphs are undirected"),
+  make_option(c("-m","--mode"), action="store", default="all",
+              help="One of all, in, out, total"),
   make_option(c("--delim","-d"), action="store", default="\t",
               help="Field delimiter in output"),
   make_option(c("-c", "--cols"), action="store", default=10, type="integer",
@@ -22,7 +22,7 @@ opt.parser <- OptionParser(option_list = option_list,
 opts <- parse_args(opt.parser, positional_arguments = TRUE)
 
 argv <- opts$args
-if(length(argv) < 2) {
+if(length(argv) < 1) {
   print_help(opt.parser)
   quit(save = "no", status = 1)
 }
@@ -31,7 +31,7 @@ N <- opts$options$cols
 
 for(net.file in argv)  {
     g <- read.graph(net.file, format="ncol", names=T, direct=T, weights="yes")
-    d <- degree.distribution(g)
+    d <- degree.distribution(g, mode=opts$options$mode)
     dd <- round(100*d[2:N],1)
     dd[N] <- round(100*sum(d[-(1:N)]),1)
 
