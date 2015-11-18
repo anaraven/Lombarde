6be5fdee44d19fe9a670425d5baba9a75d24ba28
Andres Aravena <andres.aravena@gmail.com>
2015-11-18 17:58:08 +0100

    Reorganization into folders *Preproc*, *Core*, *Analysis*, *Optimize*. Keeping code, old-code and README

--- Core/lombardemo.R
+++ Core/lombardemo.R
@@ -2,12 +2,15 @@ library(methods)
 library(igraph)
 library(parallel)
 
+change_weights <- TRUE
+base <- 10
 coexp.file <- "~/RLombarde/Input/RDB8.1/M3D/pearson/mrnet/10K.ncol"
 net.file   <- "~/RLombarde/Input/RDB8.1/Prodoric/MEME/g0-9.ncol"
 out.file   <- "/dev/stdout"
 asp.file   <- "/dev/stdout"
-change_weights <- TRUE
-base <- 10
+
+id <- out.file
+if(is.null(id)) id <- asp.file
 
 options(mc.cores=4, digits=10, scipen=3)
 
@@ -27,7 +30,7 @@ coexps <- read.table(coexp.file, as.is=TRUE, col.names=c("from", "to", "weight")
 is.valid.obs <- coexps$from %in% g.name & coexps$to %in% g.name # & coexps$from < coexps$to
 coexps <- coexps[is.valid.obs,]
 N <- nrow(coexps)
-print(paste("N:",N))
+cat(id,"N:",N,"\n")
 
 # `W` is the cost of the shortst path betwen each pair of vertices
 W <- shortest.paths(g, mode="out")
@@ -38,13 +41,12 @@ shared_pred <- mclapply(1:N, function(i) {
 			v <- rowSums(W[,unlist(coexps[i,1:2])]);
 			names(which(v==min(v)))
 })
-print(paste("shared_pred",length(shared_pred)))
+cat(id,"shared_pred:",length(shared_pred),"\n")
 
-path.extremes <- function(i, shared_pred, coexps) {
 # this function takes the i-th co-expressed pair of vertices and returns a list
 # with all the pairs of vertices that define paths connecting each common
 # predecesor to both co-expressed vertices.
-
+path.extremes <- function(i, shared_pred, coexps) {
   unlist(lapply(shared_pred[[i]],
 		function(r,v) list(c(r, v$from), c(r, v$to)), coexps[i,]),
 	 recursive=FALSE, use.names=FALSE)
@@ -55,10 +57,10 @@ path.extremes <- function(i, shared_pred, coexps) {
 expl.path <- unique(unlist(mclapply(1:N, path.extremes, shared_pred, coexps),
 			   recursive=FALSE, use.names=FALSE))
 names(expl.path) <- sapply(expl.path, paste, collapse=" ")
-print(paste("number of extremes:",length(expl.path)))
+cat(id,"number of extremes:",length(expl.path),"\n")
 # count the number of non-trivial paths
 non.trivial <- sapply(expl.path, function(e) e[1]!=e[2])
-print(table(non.trivial))
+cat(id,"non trivial ones:",sum(non.trivial),"\n")
 
 # replace each pair (a,b) for a list of all short-path-a-b
 expl.path <- mcmapply(function(e) {
@@ -69,40 +71,65 @@ expl.path <- mcmapply(function(e) {
 # which can be transformed into edges with `as.numeric(E(g, path=l))`
 
 npath <- table(sapply(expl.path, length))
-print(paste("number of paths:",sum(as.numeric(names(npath))*npath)))
-print(paste("complexity:", round(sum(log10(as.numeric(names(npath)))*npath))))
+cat(id,"number of paths:",sum(as.numeric(names(npath))*npath),"\n")
+cat(id,"complexity:", round(sum(log10(as.numeric(names(npath)))*npath)),"\n")
 
 if(!is.null(out.file)) {
+  cat(id,"Writing output","\n")
   all.edges <- lapply(unlist(expl.path[non.trivial], recursive = F),
                       function(l) E(g, path=l))
   write.graph(subgraph.edges(g, unique(unlist(all.edges))), out.file, format="ncol")
 }
 
+print.arcs <- function(edgelist) {
+  weights <- get.edge.attribute(g, "weight", edgelist)
+  sides <- get.edges(g, edgelist)
+  for(j in 1:length(edgelist)) {
+    cat("arcInVshape", vid, g.name[sides[j,1]], g.name[sides[j,2]], weights[j], "\n",
+        file=asp.file, append=TRUE)        
+  }
+}
+
 if(!is.null(asp.file)) {
+  cat(id,"Writing structure to", asp.file,"\n")
   cat("n.obs",N,"\n", file=asp.file)
   vid <- 1
-  for(extremes in names(expl.path)) {
-    for(l in 1:length(expl.path[[extremes]])) {
-      edgelist <- as.numeric(E(g, path=expl.path[[extremes]][[l]]))
-      weights <- get.edge.attribute(g, "weight", edgelist)
-      for(j in 1:length(edgelist)) {
-        cat("arcInPath\tp", vid, "\te", edgelist[j],"\t", 
-            weights[j], "\n", file=asp.file, append=TRUE, sep="")
-      }
-      expl.path[[extremes]][[l]] <- vid
-      vid <- vid+1
-    }
-  }
   for(i in 1:N){
+# <<<<<<< HEAD
     a <- coexps[i,1]
     b <- coexps[i,2]
     for(r in shared_pred[[i]]){
-      cat("shared",r,a,b,i,"\n", sep="\t",file=asp.file, append=TRUE)
-      for(j in expl.path[[paste(r,a)]]) {
-        cat("path\t",r,"\t",a,"\tp",j,"\n", sep="",file=asp.file, append=TRUE)
-      }
-      for(j in expl.path[[paste(r,b)]]) {
-        cat("path\t",r,"\t",b,"\tp",j,"\n", sep="",file=asp.file, append=TRUE)
+      cat("explanation",r,a,b,i,"\n", sep="\t",file=asp.file, append=TRUE)
+      vv1 <- expl.path[[paste(r,a)]]
+      vv2 <- expl.path[[paste(r,b)]]
+      if(is.null(vv1)) {
+        for(p2 in vv2) {
+          cat("vshape", vid, a, b, i, "\n", file=asp.file, append=TRUE)
+          edgelist <- as.numeric(E(g, path=p2))
+          print.arcs(edgelist)
+          vid <- vid + 1
+        }
+      } else if(is.null(vv2)) {
+        for(p1 in vv1) {
+            cat("vshape", vid, a, b, i, "\n", file=asp.file, append=TRUE)
+            edgelist <- as.numeric(E(g, path=p1))
+            print.arcs(edgelist)
+            vid <- vid + 1
+        }
+      } else
+# =======
+#     for(r in shared_pred[[i]]){
+#       cat("explanation",r,unlist(coexps[i,1:2]),"\n", file=asp.file, append=TRUE)
+#       vv1 <- expl.path[[r]][[ coexps[[i,1]] ]]
+#       vv2 <- expl.path[[r]][[ coexps[[i,2]] ]]
+# >>>>>>> feature/no_discret
+      for(p1 in vv1) {
+        for(p2 in vv2) {
+          cat("vshape", vid, a, b, i, "\n", file=asp.file, append=TRUE)
+          edgelist <- as.numeric(c(E(g, path=p1),E(g, path=p2)))
+          print.arcs(edgelist)
+          vid <- vid + 1
+        }
       }
     }
   }
